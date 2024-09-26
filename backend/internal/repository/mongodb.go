94d2cf375074112d237f3ff2131a7204a872d9fb
Rayzzzx <rayzzzx13@gmail.com>
2024-09-26 15:14:36 -0500

    basic set up for backend

---
+++ backend/internal/repository/mongodb.go
@@ -0,0 +1,31 @@
+package repository
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var Client *mongo.Client
+
+func ConnectDB() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	Client = client
+	log.Println("Connected to MongoDB")
+}
