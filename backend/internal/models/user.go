6a46afa197d208292c5cfffb67661929f2945b08
Rayzzzx <rayzzzx13@gmail.com>
2024-09-25 21:21:32 -0500

    add the user model

---
+++ backend/internal/models/user.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Username string             `bson:"username" json:"username"`
+	Email    string             `bson:"email" json:"email"`
+	Password string             `bson:"password" json:"-"`
+	Role     string             `bson:"role" json:"role"`
+}
